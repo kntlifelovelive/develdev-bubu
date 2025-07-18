85685dec2a2510537ea133b9496b8c326d9168a5
kntlifelovelive <kyawnyeinthant2010@gmail.com>
2025-07-18 19:31:47 +0630

    delete lsp folder and add new files

---
+++ nvim/lua/plugins/snippets.lua
@@ -0,0 +1,10 @@
+-- lua/plugins/snippets.lua
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+}
